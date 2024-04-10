73af3944e7b4bada9031f350adb48e5d250aec96
Vikasg2323 <148754361+Vikasg2323@users.noreply.github.com>
2024-04-10 13:15:29 +0700

    Update main.py

--- challenge1/main.py
+++ challenge1/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from transactions import transaction_blueprint
+from transactions import transaction_blueprint  # Assuming this is the correct import
 
 app = Flask(__name__)
 app.register_blueprint(transaction_blueprint, url_prefix="/api")
