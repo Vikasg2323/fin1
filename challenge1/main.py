4cb5e9bb711ecd9d78cd901acbb9f259794ecc4c
Vikasg2323 <148754361+Vikasg2323@users.noreply.github.com>
2024-04-16 17:28:01 +0700

    Update main.py

--- challenge1/main.py
+++ challenge1/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from transactions import transaction_blueprint  # Assuming this is the correct import
 
 app = Flask(__name__)
-app.register_blueprint(transaction_blueprint, url_prefix="/api")
+vikas
 
 if __name__ == "__main__":
     app.run(debug=True)
